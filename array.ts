7f068fb1bd95f3f5f08351ec8b877ed524da2587
zumaichi <dzumaquero@gmail.com>
2025-06-12 12:49:21 +0200

    empezar

---
+++ array.ts
@@ -0,0 +1,66 @@
+interface Carta {
+  idFoto: number;
+  imagen: string;
+}
+let cartas: Carta[] = [
+  {
+    idFoto: 1,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.jpg",
+  },
+  {
+    idFoto: 2,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.jpg",
+  },
+  {
+    idFoto: 3,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.jpg",
+  },
+  {
+    idFoto: 4,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/4.jpg",
+  },
+  {
+    idFoto: 5,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/5.jpg",
+  },
+  {
+    idFoto: 6,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/6.jpg",
+  },
+  {
+    idFoto: 7,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.jpg",
+  },
+  {
+    idFoto: 8,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.jpg",
+  },
+  {
+    idFoto: 9,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.jpg",
+  },
+  {
+    idFoto: 10,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/4.jpg",
+  },
+  {
+    idFoto: 11,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/5.jpg",
+  },
+  {
+    idFoto: 12,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/6.jpg",
+  },
+];
